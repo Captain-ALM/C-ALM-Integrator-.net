a18d37fb18e4626f5883ef497ed2d08a2af218f8
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2019-08-20 18:34:01 +0100

    Add Save/Load Interfaces
    Add the ILoader and ISaver interfaces.

---
+++ C-ALM-Integrator/C-ALM-Integrator/C-ALM-Integrator-Lib/ILoader.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alfred
+ * Date: 20/08/2019
+ * Time: 18:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace captainalm.integrator
+{
+	/// <summary>
+	/// Implements integration loader functionality.
+	/// </summary>
+	public interface ILoader
+	{
+		/// <summary>
+		/// Loads the integrator.
+		/// </summary>
+		/// <returns>Integrator</returns>
+		/// <remarks></remarks>
+		Integrator load();
+	}
+}
---
+++ C-ALM-Integrator/C-ALM-Integrator/C-ALM-Integrator-Lib/ISaver.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alfred
+ * Date: 20/08/2019
+ * Time: 18:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace captainalm.integrator
+{
+	/// <summary>
+	/// Implements integration saver functionality.
+	/// </summary>
+	public interface ISaver
+	{
+		/// <summary>
+		/// Saves the integrator.
+		/// </summary>
+		/// <param name="toSave">The integrator to save</param>
+		/// <remarks></remarks>
+		void save(Integrator toSave);
+	}
+}
