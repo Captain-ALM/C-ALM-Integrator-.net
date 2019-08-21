8be61a75765ac64a66bf9a78bc6f5627e0cfbf17
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2019-08-21 14:07:37 +0100

    Create Project
    Create the C-ALM Verifier project.

---
+++ C-ALM-Integrator/C-ALM-Integrator/C-ALM-Verifier/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alfred
+ * Date: 21/08/2019
+ * Time: 13:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace captainalm.integrator.verifier
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
