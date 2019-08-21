28013752830c5992ea8963c5ddf86a1b075086cb
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2019-08-21 09:52:58 +0100

    Add Element Base Class and Serializer Static Class
    Add the element base class that is serializable and implements IElement.
    Add a static binary serializer class.

---
+++ C-ALM-Integrator/C-ALM-Integrator/C-ALM-Integrator-Lib/Element.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alfred
+ * Date: 20/08/2019
+ * Time: 19:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace captainalm.integrator
+{
+	[Serializable]
+	/// <summary>
+	/// The Element Base Class.
+	/// </summary>
+	public abstract class Element : IElement
+	{
+		protected Object _held = null;
+		/// <summary>
+		/// Instantiates a new element base class.
+		/// </summary>
+		/// <param name="toHold">The object to hold (Must have the SerializableAttribute).</param>
+		/// <remarks></remarks>
+		public Element(Object toHold)
+		{
+			if (! object.ReferenceEquals(null, toHold)) {
+				Type tht = toHold.GetType();
+				var atrs = tht.GetCustomAttributes(typeof(SerializableAttribute), false) as SerializableAttribute[];
+				if (!object.ReferenceEquals(null, atrs)) {
+					if (atrs.Length > 0) {
+						_held = toHold;
+					} else {
+						throw new ArgumentException("toHold does not have an object with the SerializableAttribute");
+					}
+				} else {
+					throw new ArgumentException("toHold does not have an object with the SerializableAttribute");
+				}
+			} else {
+				throw new ArgumentNullException("toHold");
+			}
+		}
+
+		#region IElement implementation
+		/// <summary>
+		/// Sets the element data from the string provided.
+		/// </summary>
+		/// <param name="elementData">The element data in string form</param>
+		/// <remarks></remarks>
+		public abstract void setElementAsString(string elementData);
+		/// <summary>
+		/// Gets the string representation of the element data.
+		/// </summary>
+		/// <returns>String</returns>
+		/// <remarks></remarks>
+		public abstract string getElementAsString();
+		/// <summary>
+		/// Sets the element data from the byte array provided.
+		/// </summary>
+		/// <param name="elementData">The element data in byte array form</param>
+		/// <remarks></remarks>
+		public abstract void setElementAsByteArray(byte[] elementData);
+		/// <summary>
+		/// Gets the byte array representation of the element data.
+		/// </summary>
+		/// <returns>Byte Array</returns>
+		/// <remarks></remarks>
+		public abstract byte[] getElementAsByteArray();
+		/// <summary>
+		/// Gets or sets the held element.
+		/// </summary>
+		/// <value>The Object Contained</value>
+		///<returns>Object</returns>
+		/// <remarks></remarks>
+		public virtual object HeldElement {
+			get {
+				return _held;
+			}
+			set {
+				if (! object.ReferenceEquals(null, value)) {
+					_held = value;
+				} else {
+					throw new ArgumentNullException("value");
+				}
+			}
+		}
+		/// <summary>
+		/// Gets whether the element can be converted to and from a string.
+		/// </summary>
+		/// <value>Whether the element can be converted to and from a string</value>
+		///<returns>Boolean</returns>
+		/// <remarks></remarks>
+		public abstract bool CanBeConvertedToFromString {get;}
+		/// <summary>
+		/// Gets whether the element can be converted to and from a byte array.
+		/// </summary>
+		/// <value>Whether the element can be converted to and from a byte array</value>
+		///<returns>Boolean</returns>
+		/// <remarks></remarks>
+		public abstract bool CanBeConvertedToFromByteArray {get;}
+		/// <summary>
+		/// Gets the type of the held element.
+		/// </summary>
+		/// <value>The type of the held element</value>
+		///<returns>Type</returns>
+		/// <remarks></remarks>
+		public virtual Type HeldElementType {
+			get {
+				return _held.GetType();
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ C-ALM-Integrator/C-ALM-Integrator/C-ALM-Integrator-Lib/Serializer.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alfred
+ * Date: 20/08/2019
+ * Time: 20:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security;
+
+namespace captainalm.integrator
+{
+	/// <summary>
+	/// The Binary Serializer Class.
+	/// </summary>
+	public static class Serializer
+	{
+		private static Object slockop = new Object();
+		/// <summary>
+		/// Serializes an Object to a Byte Array.
+		/// </summary>
+		/// <param name="obj">The object to serialize</param>
+		/// <returns>Byte Array</returns>
+		/// <remarks></remarks>
+		public static Byte[] serialize(Object obj) {return serialize(new BinaryFormatter(),obj);}
+		/// <summary>
+		/// Deserializes an Object from a Byte Array.
+		/// </summary>
+		/// <param name="bts">The byte array to deserialize</param>
+		/// <returns>Object</returns>
+		/// <remarks></remarks>
+		public static Object deSerialize(Byte[] bts) {return deSerialize(new BinaryFormatter(), bts);}
+		/// <summary>
+		/// Serializes an Object to a Byte Array.
+		/// </summary>
+		/// <param name="bf">The binary formatter</param>
+		/// <param name="obj">The object to serialize</param>
+		/// <returns>Byte Array</returns>
+		/// <remarks></remarks>
+		public static Byte[] serialize(BinaryFormatter bf, Object obj) {
+			if (object.ReferenceEquals(null, bf)) {throw new ArgumentNullException("bf");}
+			if (object.ReferenceEquals(null, obj)) {throw new ArgumentNullException("obj");}
+			Byte[] toret = null;
+			lock (slockop) {
+				var ms = new MemoryStream();
+				try {
+					bf.Serialize(ms,obj);
+					toret = ms.ToArray();
+				} catch (ArgumentException e) {
+					toret = null;
+					ms.Dispose();
+					ms  = null;
+					throw e;
+				} catch (SerializationException e) {
+					toret = null;
+					ms.Dispose();
+					ms  = null;
+					throw e;
+				} catch (SecurityException e) {
+					toret = null;
+					ms.Dispose();
+					ms  = null;
+					throw e;
+				} finally {
+					if (! object.ReferenceEquals(null, ms)) {ms.Dispose();ms=null;}
+				}
+			}
+			return toret;
+		}
+		/// <summary>
+		/// Deserializes an Object from a Byte Array.
+		/// </summary>
+		/// <param name="bf">The binary formatter</param>
+		/// <param name="bts">The byte array to deserialize</param>
+		/// <returns>Object</returns>
+		/// <remarks></remarks>
+		public static Object deSerialize(BinaryFormatter bf, Byte[] bts) {
+			if (object.ReferenceEquals(null, bf)) {throw new ArgumentNullException("bf");}
+			if (object.ReferenceEquals(null, bts)) {throw new ArgumentNullException("bts");}
+			if (bts.Length < 1) {throw new ArgumentException("bts has no data");}
+			Object toret = null;
+			lock (slockop) {
+				var ms = new MemoryStream(bts);
+				try {
+					toret = bf.Deserialize(ms);
+				} catch (ArgumentException e) {
+					toret = null;
+					ms.Dispose();
+					ms  = null;
+					throw e;
+				} catch (SerializationException e) {
+					toret = null;
+					ms.Dispose();
+					ms  = null;
+					throw e;
+				} catch (SecurityException e) {
+					toret = null;
+					ms.Dispose();
+					ms  = null;
+					throw e;
+				} finally {
+					if (! object.ReferenceEquals(null, ms)) {ms.Dispose();ms=null;}
+				}
+			}
+			return toret;
+		}
+	}
+}
