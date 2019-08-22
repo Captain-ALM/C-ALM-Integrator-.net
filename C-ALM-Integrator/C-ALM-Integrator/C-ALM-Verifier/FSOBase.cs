f26a106dafbabb283ca88552803cf6ddcad42039
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2019-08-22 09:31:39 +0100

    Create FSOBase
    Create the File System object base class.

---
+++ C-ALM-Integrator/C-ALM-Integrator/C-ALM-Verifier/FSOBase.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alfred
+ * Date: 22/08/2019
+ * Time: 08:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace captainalm.integrator.verifier
+{
+	[Serializable]
+	/// <summary>
+	/// File System Object Base.
+	/// </summary>
+	public abstract class FSOBase
+	{
+		protected String path = "";
+		protected DateTime lastModified = DateTime.MinValue;
+		protected String hash = "";
+		protected Int64 size = 0;
+		
+		public FSOBase(String pathIn)
+		{
+			path = pathIn;
+		}
+		
+		public abstract void updateLastModified();
+		
+		public abstract void updateHash();
+		
+		public abstract void updateSize();
+		
+		public virtual void update() {
+			updateLastModified();
+			updateHash();
+			updateSize();
+		}
+		
+		public virtual String Path {
+			get {
+				return path;
+			}
+			set {
+				path = value;
+			}
+		}
+		
+		public virtual DateTime LastModified {
+			get {
+				return lastModified;
+			}
+		}
+		
+		public virtual String Hash {
+			get {
+				return hash;
+			}
+		}
+		
+		public virtual Int64 Size {
+			get {
+				return size;
+			}
+		}
+		
+		protected virtual Byte[] hashProcessor(Byte[] dataIn) {
+			var datlst = new List<Byte[]>();
+			var datlft = dataIn.Length;
+			var cindx = 0;
+			var toretlen = 0;
+			while (datlft > 0) {
+				if (datlft > 10485760) {
+					var buff = new Byte[10485760];
+					System.Buffer.BlockCopy(dataIn, cindx, buff, 0, buff.Length);
+					datlst.Add(hashProcess(buff));
+					toretlen += datlst[datlst.Count - 1].Length;
+					datlft -= buff.Length;
+					cindx += buff.Length;
+				} else {
+					var buff = new Byte[datlft];
+					System.Buffer.BlockCopy(dataIn, cindx, buff, 0, buff.Length);
+					datlst.Add(hashProcess(buff));
+					toretlen += datlst[datlst.Count - 1].Length;
+					datlft -= buff.Length;
+					cindx += buff.Length;
+					break;
+				}
+			}
+			var toret = new Byte[toretlen];
+			cindx = 0;
+			for (int i = 0; i < datlst.Count - 1; i++) {
+				var cbts = datlst[i];
+				System.Buffer.BlockCopy(cbts, 0, toret, cindx, cbts.Length);
+				cindx += cbts.Length;
+			}
+			return hashProcess(toret);
+		}
+		
+		protected virtual Byte[] hashProcess(Byte[] dataIn) {
+			var hp = new SHA512Managed();
+			var toret = hp.ComputeHash(dataIn);
+			hp.Dispose();
+			hp = null;
+			return toret;
+		}
+	}
+}
